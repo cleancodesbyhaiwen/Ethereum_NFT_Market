45c131e907e7480e4bc6fc4b8d5142b3f39cc265
Haiwen Zhang <81917482+cleancodesbyhaiwen@users.noreply.github.com>
2023-05-02 15:06:16 -0400

    delete passphrase

--- truffle-config.js
+++ truffle-config.js
@@ -1,16 +1,3 @@
-// infuraKey = 98e8a9488af042b492d5f6dd2314c0c7
-// memonic = height bullet neither patch pig bread noble popular type head rich engine
-// accunt address: 0x83B8A7d24006571Ddb257290B42FC47aad5D81cE
-
-// get fake ether: https://goerlifaucet.com/
-// https://goerli-faucet.pk910.de/
-
-// both ribkeby and kovan  test networks are discommissioned
-
-const HDWalletProvider = require("truffle-hdwallet-provider");
-const infuraKey = "98e8a9488af042b492d5f6dd2314c0c7";
-const mnemonic = "height bullet neither patch pig bread noble popular type head rich engine";
-
 module.exports = {
   networks: {
     development: {
